7db9038605b8b96748e160d88ce1ad7a55e8023f
michal <michal@sortivo.cz>
2014-01-02 11:33:02 +0100

    stats dao test

---
+++ src/test/java/cz/sortivo/sklikapi/StatsDAOTest.java
@@ -0,0 +1,36 @@
+package cz.sortivo.sklikapi;
+
+import java.util.List;
+
+import org.joda.time.DateTime;
+import org.junit.Test;
+
+import cz.sortivo.sklikapi.exception.InvalideRequestException;
+import cz.sortivo.sklikapi.exception.SKlikException;
+
+/**
+ *
+ * @author Michal Ličko
+ */
+public class StatsDAOTest {
+
+    @Test
+    public void testSendRequest() throws Exception{
+        Client client = new Client();
+        try{
+            client.login("USERNAME", "PASSWORD");
+        }catch(InvalideRequestException | SKlikException ex){
+            return;
+        }
+        
+        CampaignDAO campaignDAO = new CampaignDAO(client);
+        StatsDAO statsDAO = new StatsDAO(client);
+        
+        List<Campaign> campaigns = campaignDAO.listCampaigns();
+        
+        Campaign firstCamp = campaigns.get(0);
+        Stats stats = statsDAO.getStats(firstCamp, new DateTime().minusDays(10), new DateTime());
+        
+    }
+   
+}
